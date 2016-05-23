b4ac8b5658e644e0f49379422283bc07e5639cf3
Stephane BENGUIGUI <sbenguig.dev@gmail.com>
2016-05-23 23:19:13 +0200

    prepared for pundit (w/o policies/controllers/views & added Facebook connect

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,22 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   has_many :interests, dependent: :destroy
   has_many :movies, through: :interests
-#2do
+
+  def self.find_for_facebook_oauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]  # Fake password for validation
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
+      user.picture = auth.info.image
+      user.token = auth.credentials.token
+      user.token_expiry = Time.at(auth.credentials.expires_at)
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160523171104) do
+ActiveRecord::Schema.define(version: 20160523205130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,6 +98,13 @@ ActiveRecord::Schema.define(version: 20160523171104) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "address"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "picture"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "token"
+    t.datetime "token_expiry"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
