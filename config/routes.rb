5d9006d68ae62597071f5f1add340f7082aeb02a
Dimitri Bosch <dimitribosch@hotmail.com>
2016-05-25 22:53:28 +0200

    fixed the dropdown and add the watchlist route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :movies, only: [:show, :index]
   resources :users, only: [:index] 
-  get '/users/:user_id/watchlist' => 'interests#index'
+  get '/users/:user_id/watchlist' => 'interests#index', as: 'watchlist'
   resources :interests, only: [:create, :update, :destroy]
   root to: 'pages#home'
 
