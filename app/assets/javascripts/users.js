8991ecbc3895d471eca566ff0c69a9fb53aba1fa
Dimitri Bosch <dimitribosch@hotmail.com>
2016-05-26 19:52:58 +0200

    add the fb button on a modal

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,2 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$('#LogInModal').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
+
