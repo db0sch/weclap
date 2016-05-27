1b696cfde4941443b7e18166f5c36c53fdd2fc69
thomas rondio <sativva@free.fr>
2016-05-27 13:26:52 +0200

    update friends list

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,6 +33,7 @@ class User < ActiveRecord::Base
            friendslist << d['name']
         end
       end
+
       if !datadev.nil?
         datadev["data"].each do |d|
            friendslist << d['name']
@@ -45,14 +46,31 @@ class User < ActiveRecord::Base
   private
 
   def check_friendships
+      data = JSON.parse(RestClient.get "https://graph.facebook.com/#{self.uid}/invitable_friends?limit=5000&access_token=#{self.token}&l")
+      datadev = JSON.parse(RestClient.get "https://graph.facebook.com/#{self.uid}/friends?limit=5000&access_token=#{self.token}&l")
+      friendslist = []
+      if !data.nil?
+        data["data"].each do |d|
+           friendslist << d['name']
+        end
+      end
+
+      if !datadev.nil?
+        datadev["data"].each do |d|
+           friendslist << d['name']
+        end
+      end
+      self.friendslist = friendslist.to_s
+
     if !friendslist.nil?
-      JSON.parse(friendslist).each do |f|
+      JSON.parse(self.friendslist).each do |f|
         User.all.each do |u|
           if u.fullname == f
             Friendship.create({friend_id: u.id, buddy_id: self.id})
           end
         end
       end
+
     end
   end
 
