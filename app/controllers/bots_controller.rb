ff1658bdf15299e0f6bac73b401fecb691c63a1f
Dimitri Bosch <dimitribosch@hotmail.com>
2016-05-27 14:29:35 +0200

    whitelist the webhook route

--- app/controllers/bots_controller.rb
+++ app/controllers/bots_controller.rb
@@ -1,4 +1,5 @@
 class BotsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :webhook
   def webhook
      if params[‘hub.verify_token’] == “55d9c7c3cb15caf6e6c07ab217665546ed60967a84e7a267454d6197520db05406ac04b488771f65750ebc686e79e1d3a1473ef2716cf322d512a2d369f01fe6”
        render text: params[‘hub.challenge’] and return
