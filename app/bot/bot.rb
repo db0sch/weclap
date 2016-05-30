a0af69bb83cd9e5f27f31497135c075748c75500
Dimitri Bosch <dimitribosch@hotmail.com>
2016-05-30 15:45:02 +0200

    first custom cmds for the bot : list and search

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,23 +1,43 @@
 require 'facebook/messenger'
 
 Facebook::Messenger.configure do |config|
-  config.access_token = ENV['ACCESS_TOKEN']
-  config.verify_token = ENV['VERIFY_TOKEN']
+  puts "trying connection"
+  config.access_token = ENV['FB_ACCESS_TOKEN']
+  config.verify_token = ENV['FB_VERIFY_TOKEN']
 end
 
 include Facebook::Messenger
 
 Bot.on :message do |message|
+  fbid = message.sender['id']
+  user = User.where(uid: fbid)
+  response = RestClient.get "https://graph.facebook.com/v2.6/10208235975350862?fields=first_name,last_name,profile_pic&access_token=#{ENV['FB_ACCESS_TOKEN']}"
+  repos = JSON.parse(response)
+  user = User.where(first_name: repos["first_name"]).where(last_name: repos["last_name"]).first
+  p user
+
   puts "Received #{message.text} from #{message.sender}"
+
 
   case message.text
   when /hello/i
     Bot.deliver(
       recipient: message.sender,
       message: {
-        text: 'Hello, human!'
+        text: "Hello you"
       }
     )
+  when /list/i
+    interestslist = user.interests
+    interestslist.each do |interest|
+      Bot.deliver(
+        recipient: message.sender,
+        message: {
+          #text: "hello #{user.first_name}"
+          text: "#{interest.movie.title}"
+        }
+      )
+    end
   when /something humans like/i
     Bot.deliver(
       recipient: message.sender,
@@ -55,19 +75,28 @@ Bot.on :message do |message|
       }
     )
   else
-    Bot.deliver(
-      recipient: message.sender,
-      message: {
-        text: 'You are now marked for extermination.'
-      }
-    )
+    movies = Movie.where('title ILIKE ? OR original_title ILIKE ?', "%#{message.text}%", "%#{message.text}%")
+    movies.each do |movie|
+      Bot.deliver(
+        recipient: message.sender,
+        message: {
+          text: "#{movie.title}"
+        }
+      )
+    end
+    # Bot.deliver(
+    #   recipient: message.sender,
+    #   message: {
+    #     text: 'You are now marked for extermination.'
+    #   }
+    # )
 
-    Bot.deliver(
-      recipient: message.sender,
-      message: {
-        text: 'Have a nice day.'
-      }
-    )
+    # Bot.deliver(
+    #   recipient: message.sender,
+    #   message: {
+    #     text: 'Have a nice day.'
+    #   }
+    # )
   end
 end
 
