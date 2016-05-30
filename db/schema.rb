b882ad5960dfb2ae4a0100e295c3aac540605dba
yassine harraj <yassine.harraj@hec.edu>
2016-05-30 11:51:02 +0200

    added longitude and latitude to theaters

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160525170649) do
+ActiveRecord::Schema.define(version: 20160530093806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,6 +101,8 @@ ActiveRecord::Schema.define(version: 20160525170649) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "name"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "users", force: :cascade do |t|
