4af62eb460ccaacdbda71157863fcd934c90befc
thomas rondio <sativva@free.fr>
2016-05-30 13:18:18 +0200

    correct css on movie item

--- app/assets/javascripts/friendships.js
+++ app/assets/javascripts/friendships.js
@@ -1,2 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+// function addMovieToMyList() {
+  // $.ajax({
+    // console.log('hello')
+//
+  // });
+// }
+//
+// $(document).ready(function() {
+  // $('#ajax-add-movie').click(function() {
+    // addMovieToMyList();
+  // });
+// });
+//
