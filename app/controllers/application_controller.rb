28736a221cf75995852106cba541a4c7dc48517a
thomas rondio <sativva@free.fr>
2016-05-31 16:34:40 +0200

    add metatags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
-  
+
   include Pundit
   after_action :verify_authorized, except: :index, unless: :devise_controller?
   after_action :verify_policy_scoped, only: :index, unless: :devise_controller?
@@ -14,5 +14,9 @@ class ApplicationController < ActionController::Base
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to(root_path)
+  end
+
+  def default_url_options
+    { host: ENV['HOST'] || 'localhost:3000' }
   end
 end
