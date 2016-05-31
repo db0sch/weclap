eb63f44fc4451c73f17b29901c198c14568adbc8
Dimitri Bosch <dimitribosch@hotmail.com>
2016-05-31 12:08:06 +0200

    change the redis.rb file

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -12,16 +12,3 @@ if url
   end
   $redis = Redis.new(:url => url)
 end
-
-Add a second line to your Procfile
-
-# Procfile
-web: bundle exec puma -C config/puma.rb
-worker: bundle exec sidekiq -C config/sidekiq.yml
-
-Heroku Cron jobs
-
-Use the Heroku Scheduler
-
-Happy Asynchronizing!
-5
