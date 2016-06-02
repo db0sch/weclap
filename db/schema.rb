a3ab81576d377367ef99e3ac509a5db12f28563c
Stephane BENGUIGUI <sbenguig.dev@gmail.com>
2016-06-02 20:47:33 +0200

    added sidekiq in admin mode

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160530102020) do
+ActiveRecord::Schema.define(version: 20160602184306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -130,6 +130,7 @@ ActiveRecord::Schema.define(version: 20160530102020) do
     t.string   "fullname"
     t.string   "zip_code",               default: "75001"
     t.string   "city",                   default: "Paris"
+    t.boolean  "admin",                  default: false,   null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
