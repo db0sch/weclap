b22bd407430b9ce713a3a678e75baa850bc167a7
thomas rondio <sativva@free.fr>
2016-06-13 13:24:16 +0200

    correct bugs again

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,25 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  resources :movies, only: [:show, :index]
+  scope '(:locale)', locale: /fr/ do
+    resources :movies, only: [:show, :index]
+    resources :users, only: [:index] do
+      resources :friendships, only: [:show, :index]
+    end
+    resources :interests, only: [:create, :update, :destroy]
 
-  resources :users, only: [:index] do
-    resources :friendships, only: [:show, :index]
-  end
-  get '/users/:user_id/watchlist' => 'interests#index', as: 'watchlist'
-  get 'search' => 'search#index'
+    get '/users/:user_id/watchlist' => 'interests#index', as: 'watchlist'
+    get 'search' => 'search#index'
 
-  resources :interests, only: [:create, :update, :destroy]
+    authenticated :user do
+      root 'interests#index', as: :authenticated_root
+    end
+    root to: 'pages#home'
 
-  authenticated :user do
-    root 'interests#index', as: :authenticated_root
-  end
-  root to: 'pages#home'
+    mount Facebook::Messenger::Server, at: 'bot'
 
-  mount Facebook::Messenger::Server, at: 'bot'
-
-  require "sidekiq/web"
-  authenticate :user, lambda { |u| u.admin } do
-    mount Sidekiq::Web => '/sidekiq'
+    require "sidekiq/web"
+    authenticate :user, lambda { |u| u.admin } do
+      mount Sidekiq::Web => '/sidekiq'
+    end
   end
 end
