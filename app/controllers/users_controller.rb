ba378c01541b79028ee244793a7a1f18dc083205
Dimitri Bosch <dimitribosch@hotmail.com>
2016-06-26 21:38:54 +0200

    profile page for user. v0.1. still needs some work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,14 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
+  def show
+    authorize @user
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,13 @@ Rails.application.routes.draw do
     resources :interests, only: [:create, :update, :destroy]
 
     get '/users/:user_id/watchlist' => 'interests#index', as: 'watchlist'
-
     get 'search' => 'search#index'
     get 'search/autocomplete', to: 'search#autocomplete'
+
+    # No id for the profile route (user#show).
+    # It is volontary, as for now, user can access is own profile only
+    get '/profile', to: 'users#show', as: 'profile'
+
 
     authenticated :user do
       root 'interests#index', as: :authenticated_root
