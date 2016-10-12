181590a85e9dbdc6a28a59e07c336ee5eba85819
Dimitri Bosch <dimitribosch@hotmail.com>
2016-10-13 00:59:59 +0200

    starting the webhook method inside the wunderlist controller

--- app/controllers/wunderlist_controller.rb
+++ app/controllers/wunderlist_controller.rb
@@ -16,6 +16,30 @@ class WunderlistController < ApplicationController
   end
 
   def webhook
-    params
+    render nothing: true, status: 200, content_type: 'json' if params['error']
+    # CHECK The type in another (private) method
+    if params['subject']['type'] == "task"
+      case params['operation']
+      when "create"
+        p "CREATE"
+        # Trigger a bg job search the movie title
+        # Trigger a bg job which adds the movie to the user watchlist
+        # Trigger a bg job which sends the movie info to the wunderlist of the user.
+        # =====>>>> How to launch several bg job?
+        # Or do we have to launch the next bg job at the end of the previous one.
+      when "delete"
+        p "DELETE"
+        # Trigger a bg job which delete the film in the user watchlist
+      when "update"
+        p "UPDATE"
+        # Trigger a bg job search the movie title
+        # if the movie is different, then :
+          # Trigger a bg job which adds the movie to the user watchlist
+          # Trigger a bg job which sends the movie info to the wunderlist of the user.
+        # end
+      end
+    end
+
+    render nothing: true, status: 200, content_type: 'json'
   end
 end
