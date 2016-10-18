2614c19b91c3c2a684a3c50f6a1e6b7c7aefa7e9
Julien Da Silva <julien.da.silva@hotmail.com>
2016-10-18 11:45:29 +0200

    cut this

--- app/jobs/post_movie_infos_job.rb
+++ app/jobs/post_movie_infos_job.rb
@@ -1,40 +1,40 @@
-# class PostMovieInfosJob < ActiveJob::Base
-#   queue_as :default
+class PostMovieInfosJob < ActiveJob::Base
+  queue_as :default
 
-#   require 'wunderlist'
+  require 'wunderlist'
 
-#   def perform(user_id, movie = {})
-#     user = User.find(user_id)
-#     wl = wunderlist_instance(user)
-#     task = get_task(wl, user)
-#   end
+  def perform(user_id, movie = {})
+    user = User.find(user_id)
+    wl = wunderlist_instance(user)
+    task = get_task(wl, user)
+  end
 
-#   def get_task(wl, user)
-#     task = wl.tasks_by_list_id(user.wl_list_id).last  
-#   end
+  def get_task(wl, user)
+    task = wl.tasks_by_list_id(user.wl_list_id).last  
+  end
 
-#   def create_comment(task, attrs = {})
-#     task.new_comment
-#   end
+  def create_comment(task, attrs = {})
+    task.new_comment
+  end
 
-#   def update_note(wl, attrs = {})    
-#   end
+  def update_note(wl, attrs = {})    
+  end
 
-#   def new_subtask(wl, attrs = {})
-#   end
+  def new_subtask(wl, attrs = {})
+  end
 
-#   def wunderlist_instance(user)
-#     Wunderlist::API.new({
-#       access_token: user.token,
-#       client_id: ENV['WUNDERLIST_ID']
-#     })
-#   end
+  def wunderlist_instance(user)
+    Wunderlist::API.new({
+      access_token: user.token,
+      client_id: ENV['WUNDERLIST_ID']
+    })
+  end
 
 
-# $: << '.'
-# load 'wunderlist.rb'
-# wl = Wunderlist::API.new({access_token: "e12377f6b519852766b8ba6bca7dd629ddecc6bf0dd3e679b80b4599b16b", client_id: "7bcae47d189f0640df56"})
-# list = wl.list_by_id(271497561)
-# task = wl.tasks_by_list_id(list.id).last
+$: << '.'
+load 'wunderlist.rb'
+wl = Wunderlist::API.new({access_token: "e12377f6b519852766b8ba6bca7dd629ddecc6bf0dd3e679b80b4599b16b", client_id: "7bcae47d189f0640df56"})
+list = wl.list_by_id(272203097)
+task = wl.tasks_by_list_id(list.id).last
   
-# end
+end
